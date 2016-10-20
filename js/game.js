e0a1932ca6f94082aa95df020eb2ded566c0adab
Alexey Ershov <alex4rom@gmail.com>
2016-10-20 08:28:34 +0300

    Initial commit

---
+++ js/game.js
@@ -0,0 +1,56 @@
+;(function() {
+  let spriteObject = {
+    // The 'x' any 'y' source position of the sprite's image and its height and width
+    sourceX: 0,
+    sourceY: 0,
+    sourceWidth: 64,
+    sourceHeight: 64,
+
+    // The 'x' and 'y' position of the sprite on the canvas and its height and width
+    x: 0,
+    y: 0,
+    width: 64,
+    height: 64
+  };
+
+  // Canvas and context
+  let canvas = document.querySelector('canvas');
+  var ctx = canvas.getContext('2d');
+
+  // An array to store the game sprites
+  let sprites = [];
+
+  // Create sprites
+  // Center it on the canvas
+  let obj = Object.create(spriteObject);
+  obj.x = 0
+  obj.y = 168;
+  sprites.push(obj);
+
+  // Load sprite image
+  let image = new Image();
+  image.addEventListener('load', loadHandler, false);
+  image.src = 'sprites/obj.png';
+
+  function loadHandler() {
+    // Update the sprite as soon as the image has been loaded
+    update();
+  }
+
+  function update() {
+    // Create animation loop
+    window.requestAnimationFrame(update, canvas);
+
+    obj.x++;
+
+    // Render the animation
+    render();
+  }
+
+  function render() {
+    // Clear previous
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Loop through all the sprites and use their properties to display them
+  }
+})()
